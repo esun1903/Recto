b3dc0ef12b7cea6b4a0d1611ea44070c8bfed501
khyunji <klxcxl@gmail.com>
2021-04-29 16:54:54 +0900

    feat: ✨ App Main Page 가로 슬라이드 구현
    RecyclerView를 활용한 가로 슬라이드 구현

--- android/app/src/main/java/com/ssafy/recto/HomeFragment.java
+++ android/app/src/main/java/com/ssafy/recto/HomeFragment.java
@@ -1,22 +1,63 @@
 package com.ssafy.recto;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
 
     private View view;
+    private ArrayList<String> arrayList;
+    private MyAdapter adapter;
+    private RecyclerView listview;
+    private LinearLayoutManager linearLayoutManager;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.home_fragment, container, false);
+        init();
         return view;
     }
+
+    private void init() {
+        listview = view.findViewById(R.id.main_rv);
+        linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        listview.setLayoutManager(linearLayoutManager);
+
+        ArrayList<String> itemList = new ArrayList<>();
+        itemList.add("첫 번째 카드");
+        itemList.add("두 번째 카드");
+        itemList.add("세 번째 카드");
+        itemList.add("네 번째 카드");
+        itemList.add("다섯 번째 카드");
+        itemList.add("여섯 번째 카드");
+
+        adapter = new MyAdapter(getActivity(), itemList, onClickItem);
+        listview.setAdapter(adapter);
+
+        MyListDecoration decoration = new MyListDecoration();
+        listview.addItemDecoration(decoration);
+    }
+
+
+    private View.OnClickListener onClickItem = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String str = (String) v.getTag();
+            Toast.makeText(getActivity(), str, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
---
+++ android/app/src/main/java/com/ssafy/recto/MyAdapter.java
@@ -0,0 +1,61 @@
+package com.ssafy.recto;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.BreakIterator;
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private ArrayList<String> itemList;
+    private Context context;
+    private View.OnClickListener onClickItem;
+
+    public MyAdapter(Context context, ArrayList<String> itemList, View.OnClickListener onClickItem) {
+        this.context = context;
+        this.itemList = itemList;
+        this.onClickItem = onClickItem;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        // context 와 parent.getContext() 는 같다.
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.main_card_list, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String item = itemList.get(position);
+
+        holder.textview.setText(item);
+        holder.textview.setTag(item);
+        holder.textview.setOnClickListener(onClickItem);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textview;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textview = itemView.findViewById(R.id.card_tv);
+        }
+    }
+}
