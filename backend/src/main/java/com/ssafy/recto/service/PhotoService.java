0dae955c682dea550615eabeafa9844ce9391429
zzzzddz <zzzzddz@naver.com>
2021-05-03 01:24:23 +0900

    refactor: 🐙 photo & user refactoring

--- backend/src/main/java/com/ssafy/recto/service/PhotoService.java
+++ backend/src/main/java/com/ssafy/recto/service/PhotoService.java
@@ -13,37 +13,31 @@ import java.util.List;
 public class PhotoService {
 
 	private final SqlSession sqlSession;
-
-	public final PhotoDao photoDao;
-
-	public PhotoService(SqlSession sqlSession, PhotoDao photoDao) {
+	
+	@Autowired
+	public PhotoService(SqlSession sqlSession) {
 		this.sqlSession = sqlSession;
-		this.photoDao = photoDao;
 	}
 	
-	public boolean InsertPhoto(Photo photo){
+	public boolean insertPhoto(Photo photo){
 		return sqlSession.getMapper(PhotoDao.class).insertPhoto(photo) == 1;
 	}
 
-	public Photo getPhoto(int photo_seq) throws Exception{
-		Photo photo = photoDao.getPhoto(photo_seq);
-		return photo;
+	public Photo getPhoto(String photo_id) throws Exception{
+		return sqlSession.getMapper(PhotoDao.class).getPhoto(photo_id);
 	}
 	
 	public List<Photo> getPhotoList(int user_seq) throws Exception{
 		System.out.println("여기는 서비스야" + user_seq+"을 체크하려고해");
 
-		List<Photo> photo = photoDao.getPhotoList(user_seq);
-		System.out.println(photo.toString());
-		return photo;
+		return sqlSession.getMapper(PhotoDao.class).getPhotoList(user_seq);
 	}
 
-
 	public boolean modifyPhoto(Photo photo) throws Exception {
-		return photoDao.modifyPhoto(photo) == 1;
+		return sqlSession.getMapper(PhotoDao.class).modifyPhoto(photo) == 1;
 	}
 
 	public boolean deletePhoto(int photo_seq) throws Exception {
-		return photoDao.deletePhoto(photo_seq) == 1 ;
+		return sqlSession.getMapper(PhotoDao.class).deletePhoto(photo_seq) == 1 ;
 	}
 }
--- backend/src/main/java/com/ssafy/recto/service/UserService.java
+++ backend/src/main/java/com/ssafy/recto/service/UserService.java
@@ -1,6 +1,5 @@
 package com.ssafy.recto.service;
 
-import com.ssafy.recto.dao.PhotoDao;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,36 +11,32 @@ import com.ssafy.recto.util.Sha256;
 @Service
 public class UserService {
 	private final SqlSession sqlSession;
-	private final UserDao userDao;
+	
 	@Autowired
-	public UserService(SqlSession sqlSession,UserDao userDao) {
+	public UserService(SqlSession sqlSession) {
 		this.sqlSession = sqlSession;
-		this.userDao = userDao;
 	}
 	
 	public User getUser(int userId) throws Exception{
-		return userDao.getUser(userId);
+		return sqlSession.getMapper(UserDao.class).getUser(userId);
 	}
 	
 	public User userInfo(String user_id) throws Exception {
-		return userDao.userInfo(user_id);
+		return sqlSession.getMapper(UserDao.class).userInfo(user_id);
 	}
 	
 	public boolean signUp(User user) throws Exception{
         String encryptPassword = Sha256.encrypt(user.getUser_pwd());
         user.setUser_pwd(encryptPassword);
         
-        return userDao.signUp(user) == 1;
+        return sqlSession.getMapper(UserDao.class).signUp(user) == 1;
     }
 
     public boolean login(User user) throws Exception{
         String encryptPassword = Sha256.encrypt(user.getUser_pwd());
         user.setUser_pwd(encryptPassword);
-        if(userDao.login(user)!=null){
-			return true;
-		}else{
-        	return false;
-		}
+        
+        return sqlSession.getMapper(UserDao.class).login(user) == 1;
 	}
 	
 }
