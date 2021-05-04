8e9eb7e140c1cecb5f0e79adfbb7d4e17dd54dcd
신가영 <rkdud5253@naver.com>
2021-05-04 12:09:17 +0900

    feat: ✨ App 자료실 카드 상세보기 버튼기능, 다이얼로그 추가
    App 자료실 카드 상세보기 버튼기능, 다이얼로그 추가

--- android/app/src/main/java/com/ssafy/recto/PublicFragmentCardDetail.java
+++ android/app/src/main/java/com/ssafy/recto/PublicFragmentCardDetail.java
@@ -1,12 +1,14 @@
 package com.ssafy.recto;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -16,11 +18,27 @@ import androidx.fragment.app.Fragment;
 
 public class PublicFragmentCardDetail extends Fragment {
 
-    private View view;
-
+    MainActivity mainActivity;
     ImageView cardImageView;
+    ImageView info_dialog;
+    private View view;
+    private Button free_photo_card_list_btn;
+    private Context mContext;
 
-    int cardImage;
+//    int cardImage;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity)getActivity();
+        mContext = context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mainActivity = null;
+    }
 
     @Nullable
     @Override
@@ -28,6 +46,27 @@ public class PublicFragmentCardDetail extends Fragment {
         view = inflater.inflate(R.layout.public_fragment_card_detail, container, false);
 
         cardImageView = view.findViewById(R.id.card_image_detail);
+        free_photo_card_list_btn = view.findViewById(R.id.free_photo_card_list_btn);
+        info_dialog = view.findViewById(R.id.info_dialog);
+
+        free_photo_card_list_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.setFragment("public");
+            }
+        });
+
+        info_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(getContext());
+                ad.setIcon(R.drawable.question);
+                ad.setTitle("Information");
+                ad.setMessage("다운로드를 누르면 카드를 저장할 수 있습니다.");
+                ad.show();
+            }
+        });
+
 //        getData();
 //        setData();
 
