a49dccaa48e8240772da0d45e8e2458da4941a34
박예린 <linibell@naver.com>
2021-05-10 00:45:34 +0900

    feat: ✨ APP 사진형 카드 제작 API 연결
    ✅ Closes: #S04P31A204-171

--- android/app/src/main/java/com/ssafy/recto/MyApplication.java
+++ android/app/src/main/java/com/ssafy/recto/MyApplication.java
@@ -2,11 +2,19 @@ package com.ssafy.recto;
 
 import android.app.Application;
 
+<<<<<<< HEAD
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+=======
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+>>>>>>> 03ea468 (feat: ✨ APP 사진형 카드 제작 API 연결)
 public class MyApplication extends Application {
     private Boolean cardPublic;
     private Integer cardDesign;
@@ -16,4 +24,8 @@ public class MyApplication extends Application {
     private String cardDate;
     private String cardDateNum;
     private String cardPassword;
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 03ea468 (feat: ✨ APP 사진형 카드 제작 API 연결)
