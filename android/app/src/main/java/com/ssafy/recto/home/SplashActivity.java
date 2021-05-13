2773f4363beb0b7f99917357d28329dad95f470a
khyunji <klxcxl@gmail.com>
2021-05-14 04:10:46 +0900

    refactor: 🐙 APP Splash 불필요한 코드 제거
    - MyApplication 관련 불필요한 코드 제거

--- android/app/src/main/java/com/ssafy/recto/home/SplashActivity.java
+++ android/app/src/main/java/com/ssafy/recto/home/SplashActivity.java
@@ -9,16 +9,9 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.ssafy.recto.MainActivity;
 import com.ssafy.recto.R;
-import com.ssafy.recto.config.MyApplication;
 import com.ssafy.recto.user.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -26,17 +19,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser current = mFirebaseAuth.getCurrentUser();
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference("recto"); // realtime DB에서 정보 가져오기
-        // 메인 화면 문구 - 로그인 사용자
-        if (current != null) {
-            myApplication = (MyApplication) getApplication();
-        }
-        else {
-            myApplication = new MyApplication();
-        }
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
