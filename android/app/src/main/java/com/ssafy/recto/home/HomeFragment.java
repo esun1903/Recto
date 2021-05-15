5aa0d951e0692984dab50a790a84fd83423b1bc1
khyunji <klxcxl@gmail.com>
2021-05-16 02:42:03 +0900

    feat: ✨ APP 메인 화면 - 로그인 사용자 카드뷰 API 연결
    - 메인화면 - 로그인 사용자 전용 카드뷰 API 연결하여 카드 불러오기
    
    ✅ Closes: #S04P31A204-213

--- android/app/src/main/java/com/ssafy/recto/home/HomeFragment.java
+++ android/app/src/main/java/com/ssafy/recto/home/HomeFragment.java
@@ -29,6 +29,7 @@ import com.ssafy.recto.R;
 import com.ssafy.recto.api.ApiInterface;
 import com.ssafy.recto.api.CardData;
 import com.ssafy.recto.api.HttpClient;
+import com.ssafy.recto.mypage.ProfileFragmentMineAdapter;
 import com.ssafy.recto.publiccard.PublicFragmentCardDetail;
 import com.ssafy.recto.publiccard.PublicFragmentCardDetail2;
 import com.ssafy.recto.publiccard.PublicFragmentMyAdapter;
@@ -109,7 +110,7 @@ public class HomeFragment extends Fragment {
         MyAdapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View v, int pos) {
-                Log.d("확장형1 문구형2", String.valueOf(design_num[pos]));
+//                Log.d("확장형1 문구형2", String.valueOf(design_num[pos]));
 
                 if (design_num[pos] == 1) {
                     // 상세 페이지로 photo_seq 값 (sep[pos]) 보내주기
@@ -137,9 +138,16 @@ public class HomeFragment extends Fragment {
         if (current != null) {
             // 메인 화면 문구
             String nickname = sharedPreferences.getString("nickname", "RECTO의 유저");
-            Log.e("홈 닉네임 확인", nickname);
+//            Log.e("홈 닉네임 확인", nickname);
             tv_id = view.findViewById(R.id.tv_id);
             tv_id.setText(nickname + "님의 Moment");
+
+            // 메인 화면 카드뷰
+            try {
+                requestGet1();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }
 
         // 비 로그인 사용자
@@ -148,8 +156,9 @@ public class HomeFragment extends Fragment {
             tv_id = view.findViewById(R.id.tv_id);
             tv_id.setText("당신의 Moment를 기록해보세요.");
 
+            // 메인 화면 카드뷰
             try {
-                requestGet();
+                requestGet2();
             } catch (ParseException e) {
                 e.printStackTrace();
             }
@@ -158,7 +167,52 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
-    public void requestGet()  throws ParseException {
+    // 로그인 사용자 - 메인 화면 카드뷰
+    public void requestGet1() throws ParseException {
+        // 해당 user가 제작한 카드 불러오기
+        String userUid = sharedPreferences.getString("userUid", "");
+        Call<List<CardData>> call = api.getMineCard(userUid);
+
+        call.enqueue(new Callback<List<CardData>>() {
+            @Override
+            public void onResponse(Call<List<CardData>> call, Response<List<CardData>> response) {
+                String uid, video, photo, phrase, date, pwd;
+                boolean publication;
+                int design;
+                seq = new int[response.body().size()];
+                design_num = new int[response.body().size()];
+
+                photoCards.clear();
+                // 메인 화면에 카드 다섯장만 노출
+                for (int i = 0; i < 5; i++) {
+                    uid = response.body().get(i).getUser_uid();
+                    publication = response.body().get(i).isPublication();
+                    design = response.body().get(i).getDesign();
+                    video = response.body().get(i).getVideo_url();
+                    photo = response.body().get(i).getPhoto_url();
+                    phrase = response.body().get(i).getPhrase();
+                    date = response.body().get(i).getPhoto_date();
+                    pwd = response.body().get(i).getPhoto_pwd();
+
+                    photoCards.add(new CardData(uid, publication, design, video, photo, phrase, date, pwd));
+                    seq[i] = response.body().get(i).getPhoto_seq();
+                    design_num[i] = design;
+                }
+
+                adapter = new MyAdapter(getActivity(), photoCards);
+                adapter.notifyDataSetChanged();
+                listview.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<CardData>> call, Throwable t) {
+                Log.e("로그인 사용자 - 카드뷰 불러오기", "실패:<" + t.toString());
+            }
+        });
+    }
+
+    // 비 로그인 사용자 - 메인 화면 카드뷰
+    public void requestGet2() throws ParseException {
         Call<List<CardData>> call = api.getPublicCard();
 
         call.enqueue(new Callback<List<CardData>>() {
@@ -171,7 +225,7 @@ public class HomeFragment extends Fragment {
                 design_num = new int[response.body().size()];
 
                 photoCards.clear();
-                // 메인 화면에 카드 다섯장만 불러오기
+                // 메인 화면에 카드 다섯장만 노출
                 for (int i = 0; i < 5; i++) {
                     uid = response.body().get(i).getUser_uid();
                     publication = response.body().get(i).isPublication();
@@ -183,7 +237,7 @@ public class HomeFragment extends Fragment {
                     pwd = response.body().get(i).getPhoto_pwd();
 
                     photoCards.add(new CardData(uid, publication, design, video, photo, phrase, date, pwd));
-                    Log.e("photo_seq", String.valueOf(response.body().get(i).getPhoto_seq()));
+//                    Log.e("photo_seq", String.valueOf(response.body().get(i).getPhoto_seq()));
                     seq[i] = response.body().get(i).getPhoto_seq();
                     design_num[i] = design;
                 }
@@ -195,7 +249,7 @@ public class HomeFragment extends Fragment {
 
             @Override
             public void onFailure(Call<List<CardData>> call, Throwable t) {
-                Log.e("불러오기", "실패:<" + t.toString());
+                Log.e("비 로그인 사용자 - 카드뷰 불러오기", "실패:<" + t.toString());
             }
         });
     }
