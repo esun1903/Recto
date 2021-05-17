48d5409625a0b1797631966b654b73535785803e
zzzzddz <zzzzddz@naver.com>
2021-05-17 16:32:19 +0900

    fix: 👾 saveGift 버그 수정

--- android/app/src/main/java/com/ssafy/recto/arcore/PhotoVO.kt
+++ android/app/src/main/java/com/ssafy/recto/arcore/PhotoVO.kt
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor
 @NoArgsConstructor
 data class PhotoVO (
         var photo_seq : Int = 0,
-        var user_seq : String = "",
+        var user_uid : String = "",
         var photo_id : String = "",
         var photo_date : String = "",
         var photo_url : String = "",
@@ -14,8 +14,4 @@ data class PhotoVO (
         var photo_pwd : String = "",
         var design : Int = 0,
         var publication : Boolean = false
-){
-
-//    constructor(user_seq: String, photo_id: String, str: String, str2: String, cardPassword: String, cardDesign: Int, cardPublic: Boolean) : this() {
-//    }
-}
+)
